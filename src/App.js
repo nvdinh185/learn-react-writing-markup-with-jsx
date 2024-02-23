83e3acb365bfb3b233e19a9b2b973d9459fe8ba1
nvdinh185 <nvdinh0766777123@gmail.com>
2024-02-23 15:11:37 +0700

    learn-react-writing-markup-with-jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 export default function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
   )
 }
